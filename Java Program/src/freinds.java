afbe690f2459aee222ba007703b769b15bc647b3
adarsh singh <adarshsingh94516@gmail.com>
2024-07-24 12:09:06 +0530

    to print freinds name with using Array

---
+++ Java Program/src/freinds.java
@@ -0,0 +1,9 @@
+public class freinds {
+    public static void main(String[] args){
+        String name[]= {"Aman","Yash","Sharad","Ashish","Himanshu"};
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(name[i]);
+        }
+    }
+}
