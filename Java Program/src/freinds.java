de074f0f597f8f8163f916b7bd562b36af5527e7
adarsh singh <adarshsingh94516@gmail.com>
2024-07-26 09:33:30 +0530

    To generate OTP

--- Java Program/src/freinds.java
+++ Java Program/src/freinds.java
@@ -1,6 +1,7 @@
 public class freinds {
     public static void main(String[] args){
         String name[]= {"Aman","Yash","Sharad","Ashish","Himanshu"};
+        name[4]="Abhishek";
         for(int i=0;i<5;i++)
         {
             System.out.println(name[i]);
